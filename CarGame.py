f407d17cbd59752ec77a91459d9753f1ab664db4
Vítor Assis <contatoassisvt@gmail.com>
2023-03-15 18:43:24 -0300

    Update CarGame.py

--- CarGame.py
+++ CarGame.py
@@ -1,3 +1,61 @@
+# CRIANDO A BASE (1)
+# MODO DE ESCREVER UM TIPO DE COMANDO
+command = ""
+while command.lower() != "quit":
+    command = input("> ") #input introduz um comando
+    if command.lower() == "start":
+        print("Car started...")
+    elif command.lower() == "stop":
+        print("Car stopped.")
+# ESSA PARTE PODE SER REESCRITA COMO:
+command = ""
+while command != "quit":
+    command = input("> ").lower()
+    if command == "start":
+        print("Car started...")
+    elif command == "stop":
+        print("Car stopped.")
+ --
+# 'CAR GAME' (2)
+command = ""
+while command != "quit": # POSSO SIMPLIFICAR/'TIRAR' ESSE COMANDO, POIS REPETI,POSOS POR 'while True:'
+    command = input("> ").lower()
+    if command == "start":
+        print("Car started...")
+    elif command == "stop":
+        print("Car stopped.")
+    elif command == "help":
+        print("""  
+start - to start the car 
+stop - to stop the car
+quit - to quit
+        """)
+    elif command == "quit": # COMANDO REPETIDO/ COM ESSE COMANDO PODEMOS PULAR FORA DO LOOP E TERMINAR O PROGRAMA
+        break
+    else:
+        print("Sorry, I don't understand that")
+--
+# SIMPLIFICAÇÃO (3)
+# SEM A REPETIÇÃO e simmplificando FICARIA:
+command = ""
+while True: # Esse comando simplificado diz o mesmo do que o anterior: o comando ficará seguindo em loop até ser explicitamente quebrado 'break'
+    command = input("> ").lower()
+    if command == "start":
+        print("Car started...")
+    elif command == "stop":
+        print("Car stopped.")
+    elif command == "help":
+        print("""  
+start - to start the car 
+stop - to stop the car
+quit - to quit
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry, I don't understand that")
+ --
+# AJUSTES FINAIS (4)
 # O incremento dessa boolean detalhará melhor se o carro já deu start ou se já deu stop, sem mensagens repetidas "poder startar ou dar stop mais de uma vez"
 # Nosso FULL 'car game' fica assim:
 command = ""
@@ -26,3 +84,4 @@ quit - to quit
         break
     else:
         print("Sorry, I don't understand that")
+ # FIM
